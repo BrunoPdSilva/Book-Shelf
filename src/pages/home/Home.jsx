43c91c685d490baf6a4f829919be789d01844f51
Bruno Peres <bruno_16515@hotmail.com>
2022-12-12 11:43:15 -0300

    :rocket: Criando página Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,36 @@
-import { useLogout } from '../../hooks/useLogout';
 
+import { CaretDoubleLeft, CaretDoubleRight, MagnifyingGlass } from 'phosphor-react';
+import sideImage from "../../assets/home-side-image.svg";
 import './Home.css';
 
 export function Home() {
-  const { logout } = useLogout();
-
   return (
-    <div>
-      <h1 className="home">Home</h1>
+    <div className="home-page">
+      <main>
+        <header>
+          <h1>Encontre seus livros favoritos em um só lugar</h1>
+          <h2>Aqui você encontra informações sobre todos os seus livros</h2>
+
+          <div className="search-input-group">
+            <button>
+              <MagnifyingGlass size={26} color="var(--title)" />
+            </button>
+            <input
+              type="text"
+              placeholder="Procurar um livro"
+              autoComplete="none"
+            />
+          </div>
+        </header>
+        <section className="home-side-image">
+          <img src={sideImage} alt="Menina sentada lendo um livro" />
+        </section>
+      </main>
+      <div className="books-carousel">
+        <CaretDoubleLeft size={26} color="var(--title)" />
+        <section className='carousel'></section>
+        <CaretDoubleRight size={26} color="var(--title)" />
+      </div>
     </div>
   );
 }
