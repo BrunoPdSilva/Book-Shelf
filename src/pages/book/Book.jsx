cf0242164f5ef96b0873b8805f52d42e6e0ea709
Bruno Peres <bruno_16515@hotmail.com>
2022-12-20 17:04:24 -0300

    :art: Book Page -  Adicionando Sinopse

--- src/pages/book/Book.jsx
+++ src/pages/book/Book.jsx
@@ -15,7 +15,7 @@ export function Book() {
 
           <div className="buttons-container">
             <button>
-              <ShoppingCart size={22} color="#FFF" weight='bold' />
+              <ShoppingCart size={22} color="#FFF" weight="bold" />
               Comprar
             </button>
 
@@ -24,6 +24,34 @@ export function Book() {
               Download
             </button>
           </div>
+        </section>
+
+        <section className="book-details-container">
+          <h1>O Poder do Hábito</h1>
+          <h2>Charles Duhigg</h2>
+
+          <p>
+            Durante os últimos dois anos, uma jovem transformou quase todos os
+            aspectos de sua vida. Parou de fumar, correu uma maratona e foi
+            promovida. Em um laboratório, neurologistas descobriram que os
+            padrões dentro do cérebro dela mudaram de maneira fundamental.
+            Publicitários da Procter & Gamble observaram vídeos de pessoas
+            fazendo a cama. Tentavam desesperadamente descobrir como vender um
+            novo produto chamado Febreze, que estava prestes a se tornar um dos
+            maiores fracassos na história da empresa. De repente, um deles
+            detecta um padrão quase imperceptível - e, com uma sutil mudança na
+            campanha publicitária, Febreze começa a vender um bilhão de dólares
+            por anos. Um diretor executivo pouco conhecido assume uma das
+            maiores empresas norte-americanas. Seu primeiro passo é atacar um
+            único padrão entre os funcionários - a maneira como lidam com a
+            segurança no ambiente de trabalho -, e logo a empresa começa a ter o
+            melhor desempenho no índice Dow Jones. O que todas essas pessoas tem
+            em comum? Conseguiram ter sucesso focando em padrões que moldam cada
+            aspecto de nossas vidas. Tiveram êxito transformando hábitos. Com
+            perspicácia e habilidade, Charles Duhigg apresenta um novo
+            entendimento da natureza humana e seu potencial para a
+            transformação.
+          </p>
         </section>
       </main>
     </div>
