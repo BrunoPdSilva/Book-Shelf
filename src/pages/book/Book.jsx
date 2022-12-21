a327c4c96e463e9146cc2b686c14017f3dd71abd
Bruno Peres <bruno_16515@hotmail.com>
2022-12-21 17:21:17 -0300

    :art: Book Page - Ajustando layout da página e adicionando botão ver mais/menos

--- src/pages/book/Book.jsx
+++ src/pages/book/Book.jsx
@@ -4,6 +4,25 @@ import { BookButton } from './BookButton';
 import PoderDoHabito from '../../assets/poder-do-habito.svg';
 import './Book.css';
 
+const text = ` Durante os últimos dois anos, uma jovem transformou quase todos os
+aspectos de sua vida. Parou de fumar, correu uma maratona e foi
+promovida. Em um laboratório, neurologistas descobriram que os padrões
+dentro do cérebro dela mudaram de maneira fundamental. Publicitários da
+Procter & Gamble observaram vídeos de pessoas fazendo a cama. Tentavam
+desesperadamente descobrir como vender um novo produto chamado Febreze,
+que estava prestes a se tornar um dos maiores fracassos na história da
+empresa. De repente, um deles detecta um padrão quase imperceptível - e,
+com uma sutil mudança na campanha publicitária, Febreze começa a vender
+um bilhão de dólares por anos. Um diretor executivo pouco conhecido
+assume uma das maiores empresas norte-americanas. Seu primeiro passo é
+atacar um único padrão entre os funcionários - a maneira como lidam com
+a segurança no ambiente de trabalho -, e logo a empresa começa a ter o
+melhor desempenho no índice Dow Jones. O que todas essas pessoas tem em
+comum? Conseguiram ter sucesso focando em padrões que moldam cada
+aspecto de nossas vidas. Tiveram êxito transformando hábitos. Com
+perspicácia e habilidade, Charles Duhigg apresenta um novo entendimento
+da natureza humana e seu potencial para a transformação.`
+
 export function Book() {
   return (
     <div className="book-page">
@@ -22,7 +41,7 @@ export function Book() {
         </section>
 
         <section className="book-details-container">
-          <BookDetails />
+          <BookDetails title="O Poder do Hábito" author="Charles Duhigg" text={text} />
         </section>
       </main>
     </div>
--- src/pages/book/BookDetails.jsx
+++ src/pages/book/BookDetails.jsx
@@ -1,45 +1,36 @@
-import { Scroll, GlobeHemisphereWest, Buildings, Calendar } from 'phosphor-react';
+import { Scroll, GlobeHemisphereWest, Buildings, Calendar, CaretCircleUp, CaretCircleDown } from 'phosphor-react';
+import { useState } from 'react';
 
 import { BookInfo } from './BookInfo';
 import { BookCategory } from './BookCategory';
 
 import './BookDetails.css';
 
-export function BookDetails(props) {
-  const categories = [
-    'Autoajuda',
-    'Ficção Científica',
-    'Terror',
-    'História',
-    'Humor',
-    'Aventura',
-  ];
+const categories = ['Autoajuda', 'Humor', 'Aventura'];
+
+export function BookDetails({ title, author, text }) {
+  const [showMoreText, setShowMoreText] = useState(false);
 
   return (
     <>
-      <h1>O Poder do Hábito</h1>
-      <h2>Charles Duhigg</h2>
+      <div className="sumario">
+        <h1>{title}</h1>
+        <h2>{author}</h2>
+        <p>{showMoreText ? text : text.substr(0, 880)}</p>
+        <button onClick={() => setShowMoreText(!showMoreText)}>
+          {showMoreText ? (
+            <CaretCircleUp size={22} color="#FFFFFF" />
+          ) : (
+            <CaretCircleDown size={22} color="#FFFFFF" />
+          )}
+        </button>
+      </div>
 
-      <p>
-        Durante os últimos dois anos, uma jovem transformou quase todos os
-        aspectos de sua vida. Parou de fumar, correu uma maratona e foi
-        promovida. Em um laboratório, neurologistas descobriram que os padrões
-        dentro do cérebro dela mudaram de maneira fundamental. Publicitários da
-        Procter & Gamble observaram vídeos de pessoas fazendo a cama. Tentavam
-        desesperadamente descobrir como vender um novo produto chamado Febreze,
-        que estava prestes a se tornar um dos maiores fracassos na história da
-        empresa. De repente, um deles detecta um padrão quase imperceptível - e,
-        com uma sutil mudança na campanha publicitária, Febreze começa a vender
-        um bilhão de dólares por anos. Um diretor executivo pouco conhecido
-        assume uma das maiores empresas norte-americanas. Seu primeiro passo é
-        atacar um único padrão entre os funcionários - a maneira como lidam com
-        a segurança no ambiente de trabalho -, e logo a empresa começa a ter o
-        melhor desempenho no índice Dow Jones. O que todas essas pessoas tem em
-        comum? Conseguiram ter sucesso focando em padrões que moldam cada
-        aspecto de nossas vidas. Tiveram êxito transformando hábitos. Com
-        perspicácia e habilidade, Charles Duhigg apresenta um novo entendimento
-        da natureza humana e seu potencial para a transformação.
-      </p>
+      <div className="book-categories">
+        {categories.map(category => (
+          <BookCategory type={category} />
+        ))}
+      </div>
 
       <div className="book-info">
         <BookInfo title="Número de páginas" data="408 páginas">
@@ -57,10 +48,6 @@ export function BookDetails(props) {
         <BookInfo title="Data de publicação" data="24 de setembro de 2012">
           <Calendar size={24} color="var(--title)" />
         </BookInfo>
-      </div>
-
-      <div className="book-categories">
-        {categories.map(category => <BookCategory type={category} />)}
       </div>
     </>
   );
