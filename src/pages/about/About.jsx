7f411c965579534a430eeb4d131486d22a3ba7fe
Bruno Peres <bruno_16515@hotmail.com>
2023-01-03 08:39:43 -0300

    💄 Adicionando efeitos na página Contato e Sobre

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -8,8 +8,8 @@ import Framer from '../../assets/framer.png';
 import Phosphor from '../../assets/phosphor.png';
 
 import { Technology } from '../../components/Technology';
-
 import { Copyright } from 'phosphor-react';
+import { motion } from 'framer-motion';
 
 import Home from '../../assets/home-desktop.png';
 import Book from '../../assets/book-desktop.png';
@@ -18,34 +18,87 @@ import './About.css';
 
 export function About() {
   return (
-    <div className='about-page'>
-      <h1>Book Shelf</h1>
+    <div className="about-page">
+      <motion.h1
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          type: 'spring',
+          stiffness: 260,
+          damping: 20,
+          delay: 0.2,
+        }}
+      >
+        Book Shelf
+      </motion.h1>
 
-      <section className="page-images">
+      <motion.section
+        className="page-images"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          type: 'spring',
+          stiffness: 260,
+          damping: 20,
+          delay: 0.3,
+        }}
+      >
         <img src={Home} alt="" />
         <img src={Book} alt="" />
-      </section>
+      </motion.section>
 
-      <section className="technologies">
-        <Technology text="HTML" img={HTML}/>
-        <Technology text="CSS" img={CSS}/>
-        <Technology text="JavaScript" img={JavaScript}/>
-        <Technology text="Vite" img={Vite}/>
-        <Technology text="React" img={React}/>
-        <Technology text="Firebase" img={Firebase}/>
-        <Technology text="Framer" img={Framer}/>
-        <Technology text="Phosphor" img={Phosphor}/>
-      </section>
+      <motion.section
+        className="technologies"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          type: 'spring',
+          stiffness: 260,
+          damping: 20,
+          delay: 0.4,
+        }}
+      >
+        <Technology text="HTML" img={HTML} />
+        <Technology text="CSS" img={CSS} />
+        <Technology text="JavaScript" img={JavaScript} />
+        <Technology text="Vite" img={Vite} />
+        <Technology text="React" img={React} />
+        <Technology text="Firebase" img={Firebase} />
+        <Technology text="Framer" img={Framer} />
+        <Technology text="Phosphor" img={Phosphor} />
+      </motion.section>
 
-      <section className="info">
+      <motion.section
+        className="info"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          type: 'spring',
+          stiffness: 260,
+          damping: 20,
+          delay: 0.5,
+        }}
+      >
         <h3>Projeto sem fins lucrativos</h3>
-        <p>Este projeto foi criado apenas para estudar e praticar programação. SUA CÓPIA É PROIBIDA!</p>
-      </section>
+        <p>
+          Este projeto foi criado apenas para estudar e praticar programação.
+          SUA CÓPIA É PROIBIDA!
+        </p>
+      </motion.section>
 
-      <footer>
-        <Copyright size={32} color="var(--title)"/>
+      <motion.footer
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          type: 'spring',
+          stiffness: 260,
+          damping: 20,
+          delay: 0.6,
+        }}
+      >
+        <Copyright size={32} color="var(--title)" />
         <p>Copyright - Bruno Peres da Silva 2023</p>
-      </footer>
+      </motion.footer>
     </div>
-  )
+  );
 }
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,4 +1,5 @@
 import { MapPin, Envelope, WhatsappLogo } from 'phosphor-react';
+import { motion } from 'framer-motion';
 
 import ContactIMG from '../../assets/Contact.svg';
 
@@ -6,31 +7,41 @@ import './Contact.css';
 
 export function Contact() {
   return (
-    <div className="contact-page">
-      <section className="contact-info">
-        <h1>Entre em contato com a gente!</h1>
+    <div className='contact-page'>
+      <main className="contact-page-container">
+        <motion.section
+          className="contact-info"
+          initial={{ x: -600, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+        >
+          <h1>Entre em contato com a gente!</h1>
 
-        <div>
-          <MapPin size={32} color="var(--text)" />
-          <p>R. Amauri Souza, 346</p>
-        </div>
+          <div>
+            <MapPin size={32} color="var(--text)" />
+            <p>R. Amauri Souza, 346</p>
+          </div>
 
-        <div>
-          <Envelope size={32} color="var(--text)" />
-          <p>book-shelf@hotmail.com</p>
-        </div>
+          <div>
+            <Envelope size={32} color="var(--text)" />
+            <p>book-shelf@hotmail.com</p>
+          </div>
 
-        <a href="https://wa.me/5515988266126" target="_blank">
-          <button>
-            <WhatsappLogo size={32} color="#FFF" />
-            <p>WhatsApp</p>
-          </button>
-        </a>
-      </section>
+          <a href="https://wa.me/5515988266126" target="_blank">
+            <button>
+              <WhatsappLogo size={32} color="#FFF" />
+              <p>WhatsApp</p>
+            </button>
+          </a>
+        </motion.section>
 
-      <section className="img-section">
-        <img src={ContactIMG} alt="Imagem ilustrativa de contato" />
-      </section>
+        <motion.section
+          className="img-section"
+          initial={{ x: 600, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+        >
+          <img src={ContactIMG} alt="Imagem ilustrativa de contato" />
+        </motion.section>
+      </main>
     </div>
   );
 }
